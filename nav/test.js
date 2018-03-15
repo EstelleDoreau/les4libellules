b46f5f4a79b34e45f0022fa9521794cac44f9372
Estelle Doreau <doreau.e@gmail.com>
2018-03-15 23:28:37 +0100

    indentation, ménage, site fonctionnel

--- nav/test.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $(".toggle-mobile-nav").click(function() {
-        $(".toggle-mobile-nav").is(".is-active") ? ($(".toggle-mobile-nav").removeClass("is-active"), $("body").removeClass("mobile-nav")) : ($(".toggle-mobile-nav").addClass("is-active"), $("body").addClass("mobile-nav"))
-    });
-
-    $(window).scroll(function() {
-        $(window).scrollTop() >= 97 ? $("body").addClass("sticky-nav") : $("body").removeClass("sticky-nav");
-    });
-});
